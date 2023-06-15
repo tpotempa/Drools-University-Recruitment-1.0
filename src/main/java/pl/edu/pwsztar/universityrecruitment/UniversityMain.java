544070f67db0df21afb537b5821991638e1435bc
tpotempa <tpotempa@gmail.com>
2023-06-15 09:52:22 +0200

    Import change.

--- src/main/java/pl/edu/pwsztar/universityrecruitment/UniversityMain.java
+++ src/main/java/pl/edu/pwsztar/universityrecruitment/UniversityMain.java
@@ -1,9 +1,6 @@
 package pl.edu.pwsztar.universityrecruitment;
 
 import org.kie.api.KieServices;
-import org.kie.api.event.rule.DebugAgendaEventListener;
-import org.kie.api.event.rule.DebugRuleRuntimeEventListener;
-import org.kie.api.logger.KieRuntimeLogger;
 import org.kie.api.runtime.KieContainer;
 import org.kie.api.runtime.KieSession;
 
